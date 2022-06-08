0b834d85031e3254ca8c18500a7afad792c6910f
Allan Gadelha <allangadelha@gmail.com>
2022-06-07 22:41:44 -0300

    Adding components Navbar and Footer in Layout; adding component Layout in app

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,6 +1,19 @@
-export default function Layout() {
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+import Head from "next/head";
+
+export default function Layout({children}) {
 
     return (
-        <></>
+        <>
+            <Head>
+                <link rel="shortcut icon" href='/images/favicon.ico'  />
+                <title>Pokelist</title>
+            </Head>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+        </>
     )
 }
--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,13 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
-import pokeball from '../../public/images/pokeball.png';
 
 export default function Navbar() {
 
     return (
         <nav>
             <div>
-                <Image src={pokeball} width="30" height="30" alt="Pokelist" />
+                <Image src='/images/pokeball.png' width="30" height="30" alt="Pokelist" />
                 <h1>Pokelist</h1>
             </div>
             <ul>
