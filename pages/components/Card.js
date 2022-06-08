78209a31c132473647fa688eb193e97d6097e71f
Allan Gadelha <allangadelha@gmail.com>
2022-06-08 01:07:37 -0300

    CSS Card

--- pages/components/Card.js
+++ pages/components/Card.js
@@ -1,20 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import styles from '../../styles/Card.module.css';
+
 export default function Card({pokemon}) {
 
     return (
-        <div>
+        <div className={styles.card}>
             <Image
                 src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`}
                 width="120"
                 height="120"
                 alt={pokemon.name}
             />
-            <p>#{pokemon.id}</p>
-            <h3>{pokemon.name}</h3>
+            <p className={styles.id}>#{pokemon.id}</p>
+            <h3 className={styles.title}>{pokemon.name}</h3>
             <Link href={`/pokemon/${pokemon.id}`}>
-                <a>Detalhes</a>
+                <a className={styles.btn}>Detalhes</a>
             </Link>
         </div>
     )
